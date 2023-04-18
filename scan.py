b6f3b229a3b31d663d333db6ef4f0a2ddbf44a3f
Nathan <nathank1216@yahoo.com>
2023-04-18 11:59:55 -0700

    modularize and add radarr support

--- scan.py
+++ scan.py
@@ -9,92 +9,76 @@ import time
 def setupConfig():
     if (not os.path.exists("config.json")):
         shutil.copy("config/configDefault.json", "config.json")
-    
     if (not os.path.exists("lastid.json")):
         shutil.copy("config/idDefault.json", "lastID.json")
 
-def plexscan(section, paths, plexInfo):
+def plexscan(paths, plexInfo, variant):
     plexhost = plexInfo["plexhost"]
     plexport = plexInfo["plexport"]
     plextoken = plexInfo["plex-token"]
-    plexsection = plexInfo["sections"][section]
+    plexsectionID = plexInfo["sections"][variant]
 
     for path in paths:
-        url = requests.get(f"{plexhost}:{plexport}/library/sections/{plexsection}/refresh?path={urllib.parse.quote(path)}&X-Plex-Token={plextoken}").status_code
+        url = requests.get(f"{plexhost}:{plexport}/library/sections/{plexsectionID}/refresh?path={urllib.parse.quote(path)}&X-Plex-Token={plextoken}").status_code
         print(path, "code:", url)
-        time.sleep(1)
+        time.sleep(0.5)
 
-def getRadarrPaths(urlData,lastID, variant):
-    radarrhost = urlData["radarrhost"]
-    radarrAPI = urlData["radarrAPI"]
+def getArrPaths(lastIDJson, configJson, variant):
+    mappedArrVariant = configJson["mappings"][variant]
+    arrHost = configJson["arrhost"]
+    arrAPI = configJson["APIs"][variant]
+    lastID = lastIDJson[mappedArrVariant]
+
+    if (mappedArrVariant == "radarr" or mappedArrVariant == "radarr4k"):
+        mediaType = "Movie"
+    elif (mappedArrVariant == "sonarr" or mappedArrVariant == "sonarr4k"):
+        mediaType = "Series"
 
     #Limit to 1 week back, to not have a massive list of items to sort through
     today = datetime.datetime.now()
     daysAgo = (today - datetime.timedelta(days=7)).date()
+    print(f"{arrHost}/{mappedArrVariant}/api/v3/history/since?date={daysAgo}&include{mediaType}=false&apikey={arrAPI}")
+    url = requests.get(f"{arrHost}/{mappedArrVariant}/api/v3/history/since?date={daysAgo}&include{mediaType}=false&apikey={arrAPI}", verify=False).json()
 
-    if (variant == "movies"):
-        radarrAPI = urlData["radarrAPI"]
-        url = requests.get(f"{radarrhost}/radarr/api/v3/history/since?date={daysAgo}&includeMovie=false&apikey={radarrAPI}", verify=False).json()
-    elif (variant == "4k-movies"):
-        radarr4kAPI = urlData["radarr4kAPI"]
-        url = requests.get(f"{radarrhost}/radarr4k/api/v3/history/since?date={daysAgo}&includeMovie=false&apikey={radarr4kAPI}", verify=False).json()
-
-    moviePaths = []
+    mediaPaths = []
     newID = lastID
-    for movie in url:
-        if movie["id"] > lastID:
-            if (movie["data"].get("importedPath") != None):
-                fullMoviePath = (movie["data"].get("importedPath"))
-                lastSlashIndex = fullMoviePath.rfind("/")
-                trimmedPath = fullMoviePath[:lastSlashIndex+1]
-                moviePaths.append(trimmedPath)
-                if movie["id"] > newID:
-                    newID = movie["id"]
+    for media in url:
+        if media["id"] > lastID:
+            if (media["data"].get("importedPath") != None):
+                fullMediaPath = (media["data"].get("importedPath"))
+                lastSlashIndex = fullMediaPath.rfind("/")
+                trimmedPath = fullMediaPath[:lastSlashIndex+1]
+                mediaPaths.append(trimmedPath)
+                if media["id"] > newID:
+                    newID = media["id"]
+
+    lastIDJson[mappedArrVariant] = newID
+    with open('lastid.json', 'w') as lastIDFile:
+        json.dump(lastIDJson, lastIDFile)
 
     #remove duplicates
-    moviePaths = sorted(set(moviePaths))
-    return moviePaths, newID
-
+    mediaPaths = sorted(set(mediaPaths))
+    return mediaPaths
 
 def main():
-
     setupConfig()
 
     with open("config.json") as jsonConfig:
-        data = json.load(jsonConfig)
-
+        configData = json.load(jsonConfig)
     with open('lastid.json') as lastidfile:
-        lastID = json.load(lastidfile)
+        lastIDData = json.load(lastidfile)
 
     sectionList = []
-    for section in data["sections"]:
+    for section in configData["sections"]:
         sectionList.append(section)
 
     librarySelection = input(f"Which library would you like to scan? {sectionList}: ")
     while (librarySelection not in sectionList):
         print("Invalid Selection:")
         librarySelection = input(f"Which library would you like to scan? {sectionList}: ")
-
-    if (librarySelection == "movies"):
-        moviePaths, updatedID = getRadarrPaths(data, lastID["radarr"], "movies")
-
-        #Update lastid json to be the last processed item
-        lastID["radarr"] = updatedID
-        with open('lastid.json', 'w') as lastidfile:
-            json.dump(lastID, lastidfile)
-
-        plexscan(librarySelection, moviePaths, data)
-
-    if (librarySelection == "4k-movies"):
-        moviePaths, updatedID = getRadarrPaths(data, lastID["radarr4k"], "4k-movies")
-        
-        lastID["radarr4k"] = updatedID
-        with open('lastid.json', 'w') as lastidfile:
-            json.dump(lastID, lastidfile)
-
-        plexscan(librarySelection, moviePaths, data)
-
     
+    mediaPaths = getArrPaths(lastIDData, configData, librarySelection)
+    plexscan(mediaPaths, configData, librarySelection)
 
 if __name__ == "__main__":
     main()
